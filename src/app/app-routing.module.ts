ae6b0691a99aee444f7a8d7cafb2b6473324f799
jannunzi <jannunzi@gmail.com>
2020-03-10 20:31:51 -0400

    latest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CourseListComponent} from './course-list/course-list.component';
+import {CourseViewerComponent} from './course-viewer/course-viewer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CourseListComponent},
+  {path: 'course/:cid', component: CourseViewerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import {FormsModule} from '@angular/forms';
+import {CourseServiceClient} from './services/CourseServiceClient';
+import { CourseViewerComponent } from './course-viewer/course-viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    CourseViewerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    CourseServiceClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
